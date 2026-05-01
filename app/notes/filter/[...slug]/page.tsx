cea4e49b34713f93394dc4a79d25faf5490603a4
Varenik575 <invisibleredhead5@gmail.com>
2026-05-01 12:01:53 +0300

    fixed routes to [...slug]

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -3,7 +3,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import NotesClient from "@/app/notes/Notes.client";
+import NotesClient from "@/app/notes/filter/[...slug]/Notes.client";
 import { fetchNotes } from "@/lib/api";
 
 export default async function Notes() {
